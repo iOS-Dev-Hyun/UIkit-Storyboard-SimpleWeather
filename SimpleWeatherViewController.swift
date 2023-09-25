cc1bce371d6f081c17256039d16a095412f38588
LeeHyunsoo <mm083434@gmail.com>
2023-09-26 01:06:23 +0900

    초기 화면 셋팅 완료/change city button 활성화

--- SimpleWeatherViewController.swift
+++ SimpleWeatherViewController.swift
@@ -9,21 +9,30 @@ import UIKit
 
 class SimpleWeatherViewController: UIViewController {
 
+    @IBOutlet weak var cityLable: UILabel!
+    
+    @IBOutlet weak var weatherImageVIew: UIImageView!
+    
+    @IBOutlet weak var temperatureLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    let cities = ["Seoul", "Tokyo", "LA", "Seattle"]
+    let weathers = ["cloud.fill", "sun.max.fill", "wind", "cloud.sun.rain.fill"]
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changeButtonTapped(_ sender: Any) {
+        print("도시, 온도, 날씨 변경")
+        cityLable.text = cities.randomElement()
+        let imageName = weathers.randomElement()!
+        weatherImageVIew.image = UIImage(systemName: imageName)
+        
+        let randomTemp = Int.random(in: 10..<30)
+        temperatureLable.text = "\(randomTemp)°"
     }
-    */
-
+    
 }
